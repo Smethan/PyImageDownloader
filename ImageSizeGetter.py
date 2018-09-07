2378485750f5aca30f8875b09c880fac4fb17d86
smethan <ethanhargus@gmail.com>
2018-09-07 17:25:18 -0500

    Downloads working, making things pretty now

--- ImageSizeGetter.py
+++ ImageSizeGetter.py
@@ -2,6 +2,9 @@ import aiohttp
 import asyncio
 import json
 import argparse
+import uuid
+import concurrent.futures as fut
+import os
 
 class Scraper:
 	def __init__(self, session):
@@ -10,7 +13,7 @@ class Scraper:
 	@property
 	def Header(self):
 		return {
-			'User-Agent': 'ImageGetter/1.0 (Yeet)'
+			'User-Agent': 'ImageGetter/1.0 (Made by electric_espeon)'
 		}
 
 	@property
@@ -25,6 +28,10 @@ class Scraper:
 	def fileSizePropName(self):
 		return None
 
+	@property
+	def file_url(self):
+		return None
+	
 	async def grab_json(self, **kwargs):
 
 		async with self.session.get(self.baseURL + self.extension, params=kwargs, headers=self.Header) as resp:
@@ -38,6 +45,8 @@ class Scraper:
 		before_id = None
 		size = 0
 		imgcount = 0
+		file_url_list = []
+		id_list = []
 		while limit > 320:
 			limit1 = limit - 320
 			limit = 320
@@ -51,7 +60,8 @@ class Scraper:
 			for i in json:
 				filesize = i[self.fileSizePropName] if i is not None else i
 				size += filesize
-				id_list = [i["id"]]
+				id_list.append(i["id"])
+				file_url_list.append(i[self.file_url])
 			before_id = id_list[len(id_list) - 1]
 			limit = limit1
 			imgcount += len(json)
@@ -63,10 +73,28 @@ class Scraper:
 		for i in json:
 			filesize = i[self.fileSizePropName] if i is not None else i
 			size += filesize
+			file_url_list.append(i[self.file_url])
 		kilobytes = size/1024
 		megabytes = kilobytes/1024
 		
-		return size, kilobytes, megabytes, imgcount
+		return size, kilobytes, megabytes, imgcount, file_url_list
+
+	async def download(self, urls = [], tags="notag"):
+		async with aiohttp.ClientSession() as session:
+			with fut.ThreadPoolExecutor(max_workers=10000) as e:
+				print(urls)
+				if not os.path.exists(tags):
+					os.makedirs(tags)
+				for i in urls:
+					file_name, file_extension = os.path.splitext(i)
+					othername = file_name.split("/")
+					filename = othername[len(othername) - 1] + file_extension
+					print(filename)
+					response = await session.get(i, headers=self.Header)
+					img = await response.read()
+					assert response.status == 200
+					with open(os.path.join(tags, filename), "wb") as f:
+						e.submit(f.write, img)
 class e926(Scraper):
 	"""docstring for e926"""
 	
@@ -81,6 +109,9 @@ class e926(Scraper):
 	@property
 	def fileSizePropName(self):
 		return "file_size"
+	@property
+	def file_url(self):
+		return "file_url"
 	
 class e621(Scraper):
 	"""docstring for e926"""
@@ -97,6 +128,10 @@ class e621(Scraper):
 	def fileSizePropName(self):
 		return "file_size"
 
+	@property
+	def file_url(self):
+		return "file_url"
+
 class Runner:
 	
 	def parse_arg(self):
@@ -111,6 +146,7 @@ class Runner:
 		async with aiohttp.ClientSession() as session:
 			args = self.parse_arg()
 			limit = 0
+			fileList = []
 			if args["amount"] is not None:
 				limit = int(args["amount"])
 			else:
@@ -119,14 +155,18 @@ class Runner:
 				tags = ' '.join(args["tags"])
 			else:
 				tags = ""
-			if args["e926"] is not None:
-				FinalBytes, FinalKilo, FinalMega, imgcount = await e926(session).find_size(limit=limit, tags=tags)
-			elif args["e621"] is not None:
-				FinalBytes, FinalKilo, FinalMega, imgcount = await e621(session).find_size(limit=limit, tags=tags)
+			if args["e926"] is not False:
+				FinalBytes, FinalKilo, FinalMega, imgcount, fileList = await e926(session).find_size(limit=limit, tags=tags)
+			elif args["e621"] is not False:
+				FinalBytes, FinalKilo, FinalMega, imgcount, fileList = await e621(session).find_size(limit=limit, tags=tags)
 			else:
 				raise Exception("Invalid Input!")
 			await session.close()
-			print(f"Searched for {limit} images, found {imgcount} images, Bytes: {FinalBytes}, Kilobytes: {FinalKilo}, Megabytes: {FinalMega}")
+			input(f"Searched for {limit} images, found {imgcount} images, Bytes: {FinalBytes}, Kilobytes: {FinalKilo}, Megabytes: {FinalMega}\nPress any key to download, Ctrl+C to abort")
+			if args["tags"] is not None:
+				await e621(session).download(fileList, tags=args["tags"][0])
+			else:
+				await e621(session).download(fileList)
 if __name__ == "__main__":
 	r = Runner()
 	loop = asyncio.get_event_loop()
